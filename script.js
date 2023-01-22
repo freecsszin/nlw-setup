906612c4f6824160b808f631395a722ac64f13f4
Gabriel <89208080+freecsszin@users.noreply.github.com>
2023-01-22 14:05:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ form.addEventListener("change", save)
 
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
 
   const dayExists = nlwSetup.dayExists(today)
 
